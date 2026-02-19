d9ea6a44c3b143a4c3f5a6c97df976a3377f8a0c
Jiri Dusek <jiri.dusekk@gmail.com>
2026-02-19 02:24:20 +0100

    delay -> reverb

--- src/audio/AudioEngine.ts
+++ src/audio/AudioEngine.ts
@@ -114,12 +114,12 @@ export class AudioEngine {
     this.delayMix = this.audioContext.createGain();
     this.delayMix.gain.value = 1.0; // Full wet signal from delay
 
-    // Delay routing: delayBus -> delay -> feedback -> delay -> mix -> master
+    // Delay routing: delayBus -> delay -> feedback -> delay -> mix -> reverb
     this.delayBus.connect(this.delayNode);
     this.delayNode.connect(this.delayFeedback);
     this.delayFeedback.connect(this.delayNode);
     this.delayNode.connect(this.delayMix);
-    this.delayMix.connect(this.masterGain);
+    this.delayMix.connect(this.reverbBus);
 
     // Simple reverb (we'll use a basic impulse response)
     this.reverbNode = this.audioContext.createConvolver();
